c3688836023f8cd4072b47a3831c6566ad012909
yangwb <425873681@qq.com>
2020-10-03 21:49:21 +0800

    admin 熔断配置

---
+++ admin/src/main/java/com/yang/admin/config/FeignConfig.java
@@ -0,0 +1,19 @@
+package com.yang.admin.config;
+
+import feign.Retryer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class FeignConfig {
+
+    // 重试间隔100毫秒 最大重试时间1秒， 重试次数5次
+    @Bean
+    public Retryer feignRetryer() {
+        return new Retryer.Default(100, TimeUnit.SECONDS.toMillis(1L), 5);
+    }
+
+
+}
--- admin/src/main/java/com/yang/admin/feginservice/UserService.java
+++ admin/src/main/java/com/yang/admin/feginservice/UserService.java
@@ -1,16 +1,17 @@
 package com.yang.admin.feginservice;
 
+import com.yang.admin.config.FeignConfig;
 import com.yang.admin.entity.User;
+import com.yang.admin.feginservice.impl.UserServiceImpl;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @author yang
  * @since 2020/9/27
  */
-@FeignClient(value = "user-service")
+@FeignClient(value = "user-service", configuration = FeignConfig.class, fallback = UserServiceImpl.class)
 public interface UserService {
 
     @RequestMapping(value = "/user/findByUserName")
