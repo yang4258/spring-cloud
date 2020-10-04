b948352423c4b88172a56290ff4e0c7c1fd9bf7c
yangwb <425873681@qq.com>
2020-10-04 23:23:33 +0800

    config

--- monitor/src/main/java/com/yang/monitor/MonitorApplication.java
+++ monitor/src/main/java/com/yang/monitor/MonitorApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
+/**
+ * 熔断监控
+ */
 @SpringBootApplication
 @EnableTurbine
 public class MonitorApplication {
---
+++ readme.md
@@ -0,0 +1,8 @@
+zuul: 网关
+eureka1,eureka12: 注册中心
+config: 配置中心
+monitor: 熔断监控（监控多个服务）
+order-service: 服务提供方 （配置了 配置中心 获取配置文件）
+user-service: 服务提供方
+admin: 服务消费方 （fegin, ribbon, Hystrix, actuator）
+order-view: 服务消费方
