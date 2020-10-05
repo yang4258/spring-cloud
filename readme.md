2c5b236e7beff497c3cec4ee5cdc3c2591a88455
yangwb <425873681@qq.com>
2020-10-05 09:16:11 +0800

    config

--- readme.md
+++ readme.md
@@ -2,6 +2,7 @@ zuul: 网关
 eureka1,eureka12: 注册中心
 config: 配置中心
 monitor: 熔断监控（监控多个服务）
+sleuth: 服务链路追踪 zipkin
 order-service: 服务提供方 （配置了 配置中心 获取配置文件）
 user-service: 服务提供方
 admin: 服务消费方 （fegin, ribbon, Hystrix, actuator）
---
+++ sleuth/src/main/java/com/yang/sleuth/SleuthApplication.java
@@ -0,0 +1,17 @@
+package com.yang.sleuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin2.server.internal.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZipkinServer
+public class SleuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SleuthApplication.class, args);
+    }
+
+}
